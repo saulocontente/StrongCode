f57d4ca09793e5775687986de038e3fd33664429
Saulo Contente <scontente@aliancahospitalar.com>
2020-05-12 22:58:05 -0300

    create the fist scnarios steps

--- src/test/java/saulocontente/step_definitions/LoginSteps.java
+++ src/test/java/saulocontente/step_definitions/LoginSteps.java
@@ -1,5 +1,32 @@
 package saulocontente.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginSteps {
+
+@Given("^that i'm at the home page index$")
+public void thatIMAtTheHomePageIndex() {
     
+}
+
+@When("^click on the button Sign in$")
+public void clickOnTheButtonSignIn() {
+    
+}
+
+@When("^entered the loging informations$")
+public void enteredTheLogingInformations() {
+    
+}
+
+@Then("^i'm gonna be loggedin$")
+public void iMGonnaBeLoggedin() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+
+
 }
--- src/test/java/saulocontente/step_definitions/features/LoginFeature.feature
+++ src/test/java/saulocontente/step_definitions/features/LoginFeature.feature
@@ -1,1 +1,13 @@
 #Language: en
+
+Feature: Login
+As an user 
+I Want to Register my informations
+To create an user and get loggedin
+
+Scenario: Get successfully LoggedIn
+Given that i'm at the home page index
+When click on the button Sign in
+When entered the loging informations
+When click on the button Sign in
+Then i'm gonna be loggedin
