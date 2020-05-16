0970d25a8ff0b1db9d97d3ac821470faf83e1e01
Saulo Contente <scontente@aliancahospitalar.com>
2020-05-16 09:30:35 -0300

    correct imports on DSL

--- src/test/java/saulocontente/core/DSL.java
+++ src/test/java/saulocontente/core/DSL.java
@@ -3,7 +3,6 @@ package saulocontente.core;
 import static saulocontente.core.DriverFactory.getDriver;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
